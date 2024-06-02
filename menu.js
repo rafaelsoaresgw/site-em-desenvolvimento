b628e25c4cb42128f4fbb02740aeec2924062adc
rafaelsoaresgw <149423227+rafaelsoaresgw@users.noreply.github.com>
2024-06-02 19:28:16 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,15 +1,19 @@
-let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlayMenu = document.getElementById('overlay-menu');
+let btnClose = document.querySelector('.btn-fechar i');
 
-btnMenu.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu')
-})
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu');
+    overlayMenu.style.display = 'block'; // Exibir o overlay
+});
 
-menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
-overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
+btnClose.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+    overlayMenu.style.display = 'none'; // Ocultar o overlay
+});
 
+overlayMenu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+    overlayMenu.style.display = 'none'; // Ocultar o overlay
+});
